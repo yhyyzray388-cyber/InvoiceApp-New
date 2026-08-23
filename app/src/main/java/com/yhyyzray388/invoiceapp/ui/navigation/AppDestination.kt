07f31645f8547ea4233f4139618768e51378633a
yhyyzray388-cyber <yhyyzray388@gmail.com>
2026-08-24 02:27:12 +0300

    feat: add app navigation destinations

---
+++ app/src/main/java/com/yhyyzray388/invoiceapp/ui/navigation/AppDestination.kt
@@ -0,0 +1,9 @@
+package com.yhyyzray388.invoiceapp.ui.navigation
+
+sealed class AppDestination(val route: String) {
+    data object Invoices : AppDestination("invoices")
+    data object CreateInvoice : AppDestination("invoices/create")
+    data object InvoiceDetails : AppDestination("invoices/{invoiceId}") {
+        fun createRoute(invoiceId: Long) = "invoices/$invoiceId"
+    }
+}
