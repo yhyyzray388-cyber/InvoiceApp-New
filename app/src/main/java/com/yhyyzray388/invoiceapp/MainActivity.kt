ab4612ee5cda7de6ecbdbcff85159cb5730f27e0
yhyyzray388-cyber <yhyyzray388@gmail.com>
2026-08-27 03:47:29 +0300

    Connect main activity to invoice list architecture

--- app/src/main/java/com/yhyyzray388/invoiceapp/MainActivity.kt
+++ app/src/main/java/com/yhyyzray388/invoiceapp/MainActivity.kt
@@ -3,15 +3,9 @@ package com.yhyyzray388.invoiceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.yhyyzray388.invoiceapp.ui.invoices.InvoiceListScreen
+import com.yhyyzray388.invoiceapp.ui.invoices.InvoiceListViewModel
 import com.yhyyzray388.invoiceapp.ui.theme.InvoiceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val application = application as InvoiceAppApplication
+        val viewModel = ViewModelProvider(
+            this,
+            InvoiceListViewModel.Factory(application.appContainer.invoiceRepository)
+        )[InvoiceListViewModel::class.java]
+
         setContent {
             InvoiceAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    WelcomeScreen()
-                }
+                InvoiceListScreen(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-private fun WelcomeScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "InvoiceApp",
-            style = MaterialTheme.typography.headlineMedium
-        )
     }
 }
