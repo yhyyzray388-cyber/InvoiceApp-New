3879a00c03d94a89c2b368320cca756d3b7f9ccb
yhyyzray388-cyber <yhyyzray388@gmail.com>
2026-08-27 17:34:37 +0300

    Wire invoice list and create screens in MainActivity

--- app/src/main/java/com/yhyyzray388/invoiceapp/MainActivity.kt
+++ app/src/main/java/com/yhyyzray388/invoiceapp/MainActivity.kt
@@ -4,24 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.ViewModelProvider
+import com.yhyyzray388.invoiceapp.ui.invoice.CreateInvoiceScreen
+import com.yhyyzray388.invoiceapp.ui.invoice.InvoiceViewModel
 import com.yhyyzray388.invoiceapp.ui.invoices.InvoiceListScreen
 import com.yhyyzray388.invoiceapp.ui.invoices.InvoiceListViewModel
 import com.yhyyzray388.invoiceapp.ui.theme.InvoiceAppTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val application = application as InvoiceAppApplication
-        val viewModel = ViewModelProvider(
+        val app = application as InvoiceAppApplication
+        val listViewModel = ViewModelProvider(
             this,
-            InvoiceListViewModel.Factory(application.appContainer.invoiceRepository)
+            InvoiceListViewModel.Factory(app.appContainer.invoiceRepository)
         )[InvoiceListViewModel::class.java]
+        val invoiceViewModel = ViewModelProvider(
+            this,
+            InvoiceViewModel.Factory(app.appContainer.invoiceRepository)
+        )[InvoiceViewModel::class.java]
 
         setContent {
             InvoiceAppTheme {
-                InvoiceListScreen(viewModel = viewModel)
+                var showCreate = androidx.compose.runtime.remember {
+                    androidx.compose.runtime.mutableStateOf(false)
+                }
+
+                if (showCreate.value) {
+                    CreateInvoiceScreen(
+                        viewModel = invoiceViewModel,
+                        onSaved = { showCreate.value = false }
+                    )
+                } else {
+                    InvoiceListScreen(
+                        viewModel = listViewModel,
+                        onCreateInvoice = { showCreate.value = true }
+                    )
+                }
             }
         }
     }
