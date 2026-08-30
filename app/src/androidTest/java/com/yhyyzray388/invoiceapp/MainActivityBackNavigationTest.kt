247ab8795eefea7a3907769cb640b5f74e9960c2
يحيى زراعي <yhyyzray388@gmail.com>
2026-08-30 23:29:10 +0300

    Fix Back Navigation UI test import

--- app/src/androidTest/java/com/yhyyzray388/invoiceapp/MainActivityBackNavigationTest.kt
+++ app/src/androidTest/java/com/yhyyzray388/invoiceapp/MainActivityBackNavigationTest.kt
@@ -2,6 +2,7 @@ package com.yhyyzray388.invoiceapp
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
